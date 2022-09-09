61a5cd4eb99441b040b8b007da6f2723bc36fdc5
Ranjan <sahaadi99@gmail.com>
2022-09-09 17:41:03 +0530

    done

---
+++ src/main/java/com/test/electronic/MsaAzure2Application.java
@@ -0,0 +1,13 @@
+package com.test.electronic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaAzure2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaAzure2Application.class, args);
+	}
+
+}
